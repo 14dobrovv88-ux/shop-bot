65e517343c11655f872bbd28089009411ba0e983
14dobrovv88-ux <14dobrovv88@gmail.com>
2026-03-30 19:31:10 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Server is working");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
